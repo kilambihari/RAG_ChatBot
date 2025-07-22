efb11a1d1087be76576a00c87556a2437e5f8903
kilambihari <kilambihari@gmail.com>
2025-07-22 15:47:20 +0530

    Update mcp.py

--- utils/mcp.py
+++ utils/mcp.py
@@ -1,1 +1,20 @@
 
+
+def create_message(sender, receiver, msg_type, trace_id, payload):
+    return {
+        "sender": sender,
+        "receiver": receiver,
+        "type": msg_type,
+        "trace_id": trace_id,
+        "payload": payload,
+    }
+
+def parse_message(message):
+    return (
+        message["sender"],
+        message["receiver"],
+        message["type"],
+        message["trace_id"],
+        message["payload"]
+    )
+
