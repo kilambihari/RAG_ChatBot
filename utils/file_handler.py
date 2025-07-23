b5c6e5c700e4326e940b51fa4540df630c8a265b
kilambihari <kilambihari@gmail.com>
2025-07-23 11:00:03 +0530

    Update file_handler.py

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,8 +1,12 @@
 import os
 
-def save_file(uploaded_file, save_dir="uploads"):
-    os.makedirs(save_dir, exist_ok=True)
-    save_path = os.path.join(save_dir, uploaded_file.name)
-    with open(save_path, "wb") as f:
+def save_file(uploaded_file, upload_dir="uploads"):
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    
+    file_path = os.path.join(upload_dir, uploaded_file.name)
+    
+    with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
-    return save_path
+
+    return file_path
