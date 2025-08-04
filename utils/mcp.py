06e64ba2a61f0ce7022f90881cdfadb7f1fad349
kilambihari <kilambihari@gmail.com>
2025-08-04 23:35:16 +0530

    Update mcp.py

--- utils/mcp.py
+++ utils/mcp.py
@@ -1,3 +1,7 @@
+import uuid
+
+def generate_trace_id():
+    return str(uuid.uuid4())
 
 def create_message(sender, receiver, msg_type, trace_id, payload):
     return {
@@ -5,14 +9,7 @@ def create_message(sender, receiver, msg_type, trace_id, payload):
         "receiver": receiver,
         "type": msg_type,
         "trace_id": trace_id,
-        "payload": payload,
+        "payload": payload
     }
 
-def parse_message(message):
-    return (
-        message["sender"],
-        message["receiver"],
-        message["type"],
-        message["trace_id"],
-        message["payload"]
-    )
+
