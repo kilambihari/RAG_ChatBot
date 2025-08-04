0034e05692419f88da708441f5ff1e91bb5a5dbe
kilambihari <kilambihari@gmail.com>
2025-08-04 23:35:43 +0530

    Update vector_store.py

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,30 +1,22 @@
-import os
 import faiss
-import pickle
 import numpy as np
+import os
+import pickle
 
-# Save embeddings to disk
-def save_embeddings(embedding_list, texts, save_path="vector_store/index.faiss"):
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+VECTOR_STORE_PATH = "data/vector_store/"
 
-    # Convert to numpy array
-    vectors = np.array(embedding_list).astype("float32")
+def save_embeddings(doc_id, chunks, embeddings):
+    dim = len(embeddings[0])
+    index = faiss.IndexFlatL2(dim)
+    index.add(np.array(embeddings, dtype="float32"))
+    with open(os.path.join(VECTOR_STORE_PATH, f"{doc_id}.pkl"), "wb") as f:
+        pickle.dump((index, chunks), f)
 
-    # Create FAISS index
-    index = faiss.IndexFlatL2(vectors.shape[1])
-    index.add(vectors)
+def search_similar_chunks(doc_id, query):
+    from utils.embedding import get_gemini_embedding
+    with open(os.path.join(VECTOR_STORE_PATH, f"{doc_id}.pkl"), "rb") as f:
+        index, chunks = pickle.load(f)
+    query_embed = get_gemini_embedding([query])[0]
+    D, I = index.search(np.array([query_embed], dtype="float32"), 3)
+    return [chunks[i] for i in I[0]]
 
-    # Save FAISS index
-    faiss.write_index(index, save_path)
-
-    # Save text mapping
-    with open(save_path + ".pkl", "wb") as f:
-        pickle.dump(texts, f)
-
-
-# Load FAISS index and texts
-def load_embeddings(save_path="vector_store/index.faiss"):
-    index = faiss.read_index(save_path)
-    with open(save_path + ".pkl", "rb") as f:
-        texts = pickle.load(f)
-    return index, texts
